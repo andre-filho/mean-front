0546f72f64b350ee625e32962414fbc232a9ae43
Andre Filho <andre.filho001@outlook.com>
2018-12-15 11:17:52 -0200

    chore(posts): add material and posts list and header

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   templateUrl: './post-create.component.html',
   selector: 'app-post-create',
+  styleUrls: ['./post-create.component.css'],
 })
 
 export class PostCreateComponent {
